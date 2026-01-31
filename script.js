a29d499ba1c862b9443c69757953fb208b097f1e
Noah Gullo <noahgulloingram@gmail.com>
2026-01-30 23:38:15 -0800

    Add: basic book layout and render test books

--- script.js
+++ script.js
@@ -14,22 +14,56 @@ function Book(title, author, numPages, beenRead, id){
     this.getTitle = function(){
         return this.title;
     }
+
+    this.getAuthor = function(){
+        return this.author;
+    }
+
+    this.getNumPages = function(){
+        return this.numPages;
+    }
+
+    this.getBeenRead = function(){
+        return this.beenRead;
+    }
+
+    this.getId = function(){
+        return this.id;
+    }
 }
 
 
 function addBookToLibrary(title, author, numpages, beenRead){
     const newBook = new Book(title, author, numpages, beenRead, crypto.randomUUID());
     myLibrary.push(newBook);
+    displayBooks();
 }
 
 function displayBooks(){
+    const card_container = document.getElementById("card-container");
+    if(card_container.childElementCount > 0){
+        while(card_container.firstChild){
+            card_container.removeChild(card_container.lastChild);
+        }
+    }
+
     for(let i = 0; i < myLibrary.length; i++){
-        console.log("Added '" + myLibrary[i].getTitle() + "' to the array.");
+        const currentBook = myLibrary[i];
+        const uuid = currentBook.getId();
+
+        const book = document.createElement("div");
+        book.setAttribute("id", uuid);
+        book.setAttribute("class", "book");
+
+        const titleText = document.createElement("p");
+        titleText.setAttribute("class", "title-text");
+        titleText.textContent = currentBook.getAuthor();
+
+        card_container.appendChild(book);
+        document.getElementById(uuid).appendChild(titleText);
     }
 }
 
-const book1 = addBookToLibrary("How to Kill a Mockingbird", "Harper Lee", 250, true, crypto.randomUUID());
-const book2 = addBookToLibrary("War and Peace", "Leo Toltstoy", 750, false);
-const book3 = addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 342, true);
-
-displayBooks();
+for(let i = 0; i < 10; i++){
+    const book = addBookToLibrary("title", i, 100, true, crypto.randomUUID)
+}
