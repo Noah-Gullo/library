c028435d6580c2d02dd7d640314a865a5b345b66
Noah Gullo <noahgulloingram@gmail.com>
2026-01-30 22:25:27 -0800

    Add: button with styling

--- script.js
+++ script.js
@@ -20,13 +20,16 @@ function Book(title, author, numPages, beenRead, id){
 function addBookToLibrary(title, author, numpages, beenRead){
     const newBook = new Book(title, author, numpages, beenRead, crypto.randomUUID());
     myLibrary.push(newBook);
-    console.log("Added '" + newBook.getTitle() + "' to the array.");
 }
 
 function displayBooks(){
-    
+    for(let i = 0; i < myLibrary.length; i++){
+        console.log("Added '" + myLibrary[i].getTitle() + "' to the array.");
+    }
 }
 
 const book1 = addBookToLibrary("How to Kill a Mockingbird", "Harper Lee", 250, true, crypto.randomUUID());
 const book2 = addBookToLibrary("War and Peace", "Leo Toltstoy", 750, false);
 const book3 = addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 342, true);
+
+displayBooks();
